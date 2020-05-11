11f9ab59d6b69b6f0dfc4cccaef67485a5f4c2f6
Amos Park <amospark@Jessicas-MacBook-Air.local>
2020-05-11 12:44:09 -0600

    added section 4 scripts

---
+++ section-4/forloop.py
@@ -0,0 +1,7 @@
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician)
+
+# the 'for magician' loop tells Python to pull a name from the list 'magicians' and associate it with the variable magician. Python then repeats lines 2 and 3 once for each name in the list.  it might help to read, "For every magician in the list of magicians, print the magician's name."
+
+
---
+++ section-4/range.py
@@ -0,0 +1,4 @@
+for value in range(1, 5):
+    print(value)
+
+# range function to generate a series of numbers. range(1, 5) will print from 1 thru 4.  if you want to count from 0, use, range(5), that will print from 0 thru 4
