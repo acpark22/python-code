6f059be6b91009cd1949ad73cd7443eaef9b339a
Amos Park <amospark@Jessicas-MacBook-Air.local>
2020-05-12 14:35:41 -0600

    added addtl section 5 scripts

---
+++ section-5/mult_ifs.py
@@ -0,0 +1,12 @@
+requested_toppings = ['pepporoni', 'sausage']
+if 'pepporoni' in requested_toppings:
+    print("Adding pepporoni.")
+if 'mushrooms' in requested_toppings:
+    print("Adding mushrooms.")
+if 'sausage' in requested_toppings:
+    print("Adding sausage.")
+
+print("\nFinished making your pizza!")
+
+
+
