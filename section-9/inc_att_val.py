6b202a7fc263776031995bfa52b63a56f05fee19
Amos Park <amospark@Jessicas-MacBook-Air.local>
2020-05-29 14:56:23 -0600

    addtl section 9 scripts

---
+++ section-9/inc_att_val.py
@@ -0,0 +1,63 @@
+class Car:
+    """Represent a car."""
+    def __init__(self, make, model, year):
+        """Initialize attributes to describe a car."""
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        """Return a neatly formatted descriptive name."""
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+
+    def read_odometer(self):
+        """Print a stmnt showing the car's mileage."""
+        print(f"This car has {self.odometer_reading} miles on it.")
+
+    def update_odometer(self, mileage):
+        """
+        Set the odometer reading to the given value.
+        Reject the change if it attempts to roll the odometer back.
+        """
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+
+    def increment_odometer(self, miles):
+        """Add the given amount to the odometer reading."""
+        self.odometer_reading += miles
+
+my_used_car = Car('subaru', 'outback', 2015)
+print(my_used_car.get_descriptive_name())
+
+my_used_car.update_odometer(23_500)
+my_used_car.read_odometer()
+
+my_used_car.increment_odometer(100)
+my_used_car.read_odometer()
+
+#the new method, 'increment_odometer() takes in a number of miles, and adds this value to 'self.odometer_reading. 
+#we set its odometer to 23500 by calling update_odometer() and passing it 23500. 
+#at 'increment_odometer()' and pass it it 100 to add the 100 miles that we drove btwn buying the car and registering it.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
